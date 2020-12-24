d10b17bae150c7a896f99adcad4f40f128da0017
Md Salim Hasan Riad <61575024+Salim66@users.noreply.github.com>
2020-12-24 16:28:20 +0600

    Responsive Navbar Toggle Effect

---
+++ app.js
@@ -0,0 +1,12 @@
+//Selector 
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+//Event Listener
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('open');
+    links.forEach(link => {
+        link.classList.toggle('fade');
+    });
+});
